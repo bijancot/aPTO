b36193046040bebc84f31d03160dab2ac4832b4a
bijancot <panjidia995@gmail.com>
2019-06-02 18:32:15 +0700

    edit database

--- req/database.php
+++ req/database.php
@@ -7,14 +7,14 @@ $host = "172.17.0.4";
 
 $mysqli = new mysqli($host,$user,$pass,$db);
 
-$id = "xc";
-$q = $mysqli->prepare("select idtagihan from apto_tagihan where idtagihan !=?");
-$q->bind_param('s',$id);
-$q->execute();
-$q->bind_result($idtagihan);
+// $id = "xc";
+// $q = $mysqli->prepare("select idtagihan from apto_tagihan where idtagihan !=?");
+// $q->bind_param('s',$id);
+// $q->execute();
+// $q->bind_result($idtagihan);
 
 
-while($q->fetch()){
-    echo $idtagihan;
-}
+// while($q->fetch()){
+//     echo $idtagihan;
+// }
 ?>
